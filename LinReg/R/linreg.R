c0ff964f0086e31e8dbfd8297279cf12c5202d20
Alexander Karlsson <aleka769@student.liu.se>
2018-09-18 12:14:12 +0200

    updated version of linreg function, with list elements for methods (guslu)

--- LinReg/R/linreg.R
+++ LinReg/R/linreg.R
@@ -1,3 +1,16 @@
 linreg <- function(formula, data){
-  paste("hej")
+  f <- formula(formula)
+  y <- all.vars(f)[1]
+  X <- model.matrix(f, data)
+  
+  
+  #return:
+  retlist <- list("beta_hat"      = bhat,
+                  "fits"          = yhat,
+                  "residuals"     = resid,
+                  "std_residuals" = stdresid,
+                  "df"            = df,
+                  "call"          = f)
 }
+
+
