5188a8759a01dd3840c915e3f3967db4be6bfa1e
Alexander Karlsson <aleka769@student.liu.se>
2018-09-18 14:59:12 +0200

    function return list is fixed according to guslu's RC list in separate R-script

--- LinReg/R/linreg.R
+++ LinReg/R/linreg.R
@@ -1,29 +1,33 @@
 linreg <- function(formula, data){
-  # Set-up of matrices and formula:
-  f      <- formula(formula)
-  yname  <- all.vars(f)[1]
-  X      <- model.matrix(f, data)
-  y      <- data[,yname]
+  
+  # Set-up of vars, matrices and formula:
+  f       <- formula(formula)
+  yname   <- all.vars(f)[1]
+  X       <- model.matrix(f, data)
+  y       <- data[,yname]
+  
   
   # Matrix algebra:
-  bhat   <- solve(t(X) %*% X) %*% t(X) %*% y
-  yhat   <- X %*% bhat
-  resid  <- y - yhat
-  df     <- nrow(X) - (ncol(X) - 1)
-  resvar <- as.vector((t(resid) %*% resid) / df)
-  varb   <- resvar * solve(t(X) %*% X)
-  tbeta  <- bhat / sqrt(diag(varb))
-  pbeta <- 1 - pt(q = tbeta, df = df)
+  bhat    <- solve(t(X) %*% X) %*% t(X) %*% y
+  yhat    <- as.vector(X %*% bhat)
+  resid   <- y - yhat
+  df      <- nrow(X) - (ncol(X) - 1)
+  resvar  <- as.vector((t(resid) %*% resid) / df)
+  varb    <- resvar * solve(t(X) %*% X)
+  tbeta   <- bhat / sqrt(diag(varb))
+  pbeta   <- 1 - pt(q = tbeta, df = df)
   
-  # Return list, class RC:
-  retlist <- setRefClass("linreg",
-                         fields = list("beta_hat"  = bhat,
-                                       "fits"      = yhat,
-                                       "residuals" = resid,
-                                       "df"        = df,
-                                       "call"      = f,
-                                       "p-values"  = pbeta))
+  
+  # Return list, input to RC list ("linreg_class"):
+  retlist <- linreg_class$new(beta_hat  = bhat,
+                              fits      = yhat,
+                              residuals = resid,
+                              df        = df,
+                              call      = f)
+  
+  # Return
   return(retlist)
 }
 
-linreg(formula = Petal.Length ~ Petal.Width, data = iris)
+# Test:
+# linreg(formula = Sepal.Length ~ Sepal.Width, data = iris)
