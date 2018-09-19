2dbbed3f433348bec2371df87a88b89ab604aab5
napoleongl <guslu389@student.liu.se>
2018-09-20 00:27:41 +0200

    changed descriptions and modified summaryfunction

--- LinReg/R/linreg_class.R
+++ LinReg/R/linreg_class.R
@@ -2,26 +2,20 @@
 #'
 #' @description linreg object holds data and methods for data calculated in \code{linreg()}-function.
 #'
-#' @field beta_hat Estimates for each \beta
+#' @field beta_hat Estimates for each beta
 #' @field fits Contains fitted values
 #' @field residuals Contains residuals, i.e. observed - fitted
 #' @field resid_var Residual variance
-#' @field beta_info Contains variance, t-value and p-value for each estimated \beta value
+#' @field beta_info Contains variance, t-value and p-value for each estimated beta value
 #' @field df Degrees of freedom for the model
 #' @field formula Formula used in model
 #' @field call Call sent to \code{linreg()}
 #'
-#' @describeIn print Prints the cal and coefficient estimates
-#' @describeIn summary Prints a summary of the model including esimates and their variances, t-values and p-values as well as the degrees of freedom and residual variance
-#' @describeIn plot Produces plots of residuals and standardized residuals vs fitted values
-#' @describeIn coef Returns \beta estimates
-#' @describeIn resid Returns residual vector from the model
-#' @describeIn pred Retrns fitted values from the model
 #'
 #' @export
 
 linreg_class <- setRefClass("linreg",
-  fields = list(beta_hat      = "snumeric",
+  fields = list(beta_hat      = "numeric",
                 fits          = "numeric",
                 residuals     = "numeric",
                 resid_var     = "numeric",
@@ -44,6 +38,7 @@ linreg_class$methods(
       .self$call <- call
       },
 
+#' @describeIn print Prints the cal and coefficient estimates
   "print" = function(){
       cat("Call:\n",
           deparse(call),
@@ -54,18 +49,22 @@ linreg_class$methods(
       format(signif(beta_hat,3))
       },
 
+#' @describeIn coef Returns beta estimates
   "coef" = function(){
       beta_hat
       },
 
+#' @describeIn resid Returns residual vector from the model
   "resid" = function(){
       residuals
       },
 
+#' @describeIn pred Retrns fitted values from the model
   "pred" = function(){
       fits
       },
 
+#' @describeIn plot Produces plots of residuals and standardized residuals vs fitted values
   "plot" = function(){
       plot_data <- data.frame(residuals,
                               fits,
@@ -94,26 +93,21 @@ linreg_class$methods(
       gridExtra::grid.arrange(res_vs_fitted, std_vs_fitted, nrow = 2)
   },
 
+#' @describeIn summary Prints a summary of the model including esimates and their variances, t-values and p-values as well as the degrees of freedom and residual variance
   "summary" = function(){
-      beta_data <- cbind("Estimate"=beta_hat,
-                         "Std.Err" = sqrt(beta_info$Variance),
-                         "t_value" = beta_info$t_value,
-                         "p_value" = beta_info$p_value
+      beta_data <- cbind("Estimate"= round(beta_hat,3),
+                         "Std.Err" = round(sqrt(beta_info$Variance),3),
+                         "t_value" = round(beta_info$t_value,3),
+                         "p_value" = round(beta_info$p_value,3)
                          )
-      # Removed since df's/vectors are only printed of they are called last...
-      # resid_data <- c(quantile(residuals, probs = c(0,0.25,0.5,0.75,1)))
-      # names(resid_data) <- c("Min","Q1","Median","Q3","Max")
 
-      cat("Call:\n",
-          deparse(call),
-          "\n\nVariance:",
-          signif(resid_var,3),
-          ",   Degrees of freedom: ",
+      cat(paste(capture.output(beta_data),sep="\n")[-1],sep="\n")
+
+      cat("Residual standard error: ",
+          signif(sqrt(resid_var),3),
+          "on ",
           df,
-          "\n\nCoefficients:\n",
-          sep = ""
-      )
-
-      format(round(beta_data,2))
+          " degrees of freedom"
+          )
   }
 )
