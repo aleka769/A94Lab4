2f68754d2060dc8960cdfce68ff25b7c7c52d99d
Alexander Karlsson <aleka769@lisam.liu.se>
2018-09-20 20:08:49 +0200

    output from LinReg.Rmd added in vignettes. Added 'methods' to 'Imports:'-line in DESCRIPTION

---
+++ LinReg/vignettes/LinReg.R
@@ -0,0 +1,7 @@
+## ---- echo = FALSE, message = FALSE--------------------------------------
+knitr::opts_chunk$set(collapse = T, comment = "#>")
+options(tibble.print_min = 4L, tibble.print_max = 4L)
+library(dplyr)
+library(ggplot2)
+set.seed(1014)
+
