80c52c6928ca3c65271f2b7cf12a429f1d7e96ae
Faton R <fatonr@gmail.com>
2018-09-20 17:09:45 +0200

    Completed theme_liu(), added import ggplot2 in DESCRIPTION

--- LinReg/R/liu_theme.R
+++ LinReg/R/liu_theme.R
@@ -1,28 +1,27 @@
-theme_liu <- function(data){
+#' Linkoping University theme
+#' @importFrom ggplot2 theme_bw theme element_line element_rect element_blank element_text margin
+#' 
+#' @return A ggplot theme
+#' @export
+theme_liu <- function(){
   
-  # Need to change to aes_string()
-  
-  p <- ggplot(data = iris, aes(x = Sepal.Length, y = Petal.Width, color = Species)) +
-    annotation_custom(grid::rasterGrob(test)) +
-    geom_point() +
-    theme_bw() +
-    theme(panel.grid.major = element_line(color = "#16C7D2", 
-                                          linetype = 3),
-          panel.grid.minor = element_blank(),
-          panel.background = element_rect(), 
-          plot.background = element_rect(),
-          panel.border = element_blank(),
-          text = element_text(color = "#16C7D2"),
-          axis.text = element_text(color = "#16C7D2"),
-          axis.ticks = element_line(color = "#16C7D2"),
-          axis.line.x.bottom = element_line(color = "#16C7D2", 
-                                            arrow = arrow(length = unit(0.1, "inches"), type = "closed")),
-          axis.line.y.left = element_line(color = "#16C7D2", 
-                                          arrow = arrow(length = unit(0.1, "inches"), type = "closed")),
-          axis.title.y = element_text(angle = 0),
-          axis.title.x = element_text(hjust = 1),
-          plot.margin=margin(5,5,30,5)) +
-    labs(title = "Linköpings universitet")
+  ggplot2::theme_bw() +
+  ggplot2::theme(panel.grid.major = ggplot2::element_line(color = "#16C7D2", 
+                                                          linetype = 3), 
+                 panel.grid.minor = ggplot2::element_blank(),
+                 panel.background = ggplot2::element_rect(), 
+                 plot.background = ggplot2::element_rect(),
+                 panel.border = ggplot2::element_blank(),
+                 text = ggplot2::element_text(color = "#16C7D2"),
+                 axis.text = ggplot2::element_text(color = "#16C7D2"),
+                 axis.ticks = ggplot2::element_line(color = "#16C7D2"),
+                 axis.line.x.bottom = ggplot2::element_line(color = "#16C7D2", 
+                                                            arrow = grid::arrow(length = grid::unit(0.1, "inches"), type = "closed")),
+                 axis.line.y.left = ggplot2::element_line(color = "#16C7D2", 
+                                                          arrow = grid::arrow(length = grid::unit(0.1, "inches"), type = "closed")),
+                 axis.title.y = ggplot2::element_text(angle = 0),
+                 axis.title.x = ggplot2::element_text(hjust = 1),
+                 plot.margin= ggplot2::margin(5,5,30,5))
   
 }
 
