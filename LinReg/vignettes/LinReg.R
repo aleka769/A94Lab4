3334344c45b244c3fb95e917c0b60bda948179ea
Alexander Karlsson <aleka769@lisam.liu.se>
2018-09-21 09:59:16 +0200

    changes in vignettes due to changes in code...

--- LinReg/vignettes/LinReg.R
+++ LinReg/vignettes/LinReg.R
@@ -1,9 +1,16 @@
-## ---- echo = FALSE, message = FALSE--------------------------------------
+## ---- echo = FALSE, message = FALSE, warning=FALSE-----------------------
 knitr::opts_chunk$set(collapse = T, comment = "#>")
 library(ggplot2)
+library(gridExtra)
 library(LinReg)
 set.seed(1014)
 
 ## ---- echo = TRUE, message = FALSE---------------------------------------
-linreg(formula = Sepal.Width ~ Sepal.Length, data = iris)
+mod <- linreg(formula = Sepal.Width ~ Sepal.Length, data = iris)
+
+## ---- echo = TRUE, message = FALSE, warning=FALSE------------------------
+print(mod)
+
+## ---- echo = TRUE, message = FALSE---------------------------------------
+plot(mod)
 
